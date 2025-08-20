90b50869b633ed1bcaf2493cee8e7617a21c29fe
Nazare Almeida <nazyalmeida@gmail.com>
2025-08-20 14:00:10 -0300

    alterações no vite

--- init-dev/client/src/api/courseService.js
+++ init-dev/client/src/api/courseService.js
@@ -1,5 +1,5 @@
-// Este é um serviço de exemplo.
-// Por enquanto, não faremos requisições a API, mas a estrutura já está pronta.
+// Exemplo:
+// Por enquanto, não fiz requisições a API, mas a estrutura já está pronta.
 const getCourses = () => {
   return [
     { id: 1, title: 'Lógica de Programação', description: 'Fundamentos essenciais.' },
